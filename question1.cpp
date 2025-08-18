98cd385b6b9c0c9b247cc4bd2d2f4ec92a76476e
rslariya <rslariya_be24@thapar.edu>
2025-08-18 21:24:26 +0530

    Update question1.cpp

--- question1.cpp
+++ question1.cpp
@@ -1,1 +1,42 @@
-q
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5];
+    cout << "Enter five values for Array: ";
+    for (int i=0;i<=4;i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "The array is :\n ";
+    for (int i=0;i<=4;i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
+
+//create and display done
+
+1.c ---- INSERT
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5];
+    int size = 5; 
+ 
+    cout << "Enter six values for Array: ";
+    for (int i = 0; i < size+1; i++) {  
+        cin >> arr[i];
+    }
+    cout << "The array is :\n ";
+    for (int i = 0; i < size+1; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
+
